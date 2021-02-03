2f08621c9dd777cfd3117667449d6031337eba97
Zachetka <axi83@mail.ru>
2021-02-03 09:12:24 +0300

    add section works

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,11 @@ $(function() {
         arrows: false,
         dots: true,
     });
+
+    $('.works__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-arrow--prev"><img src="assets/images/icons/arrow-left.svg" alt="icon: arrow-left"></button>',
+        nextArrow: '<button class="slick-arrow slick-arrow--next"><img src="assets/images/icons/arrow-right.svg" alt="icon: arrow-right"></button>'
+    });
 });
